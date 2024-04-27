71df7697de9553034776d4b4b4815a420282f03e
unknown <mutiasarijuli@gmail.com>
2024-04-27 10:38:37 +0700

    Jawaban Soal Nomor 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,6 +21,23 @@
           <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
           <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/produk">Produk</a>
+            </li>
+          </ul>
+          <form class="d-flex mt-3" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </div>
+
         
   </nav>
   <div class="container">
