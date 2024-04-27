1150751666504f07acebf6e8e3e8b921b9e133c2
unknown <mutiasarijuli@gmail.com>
2024-04-27 10:44:54 +0700

    Jawaban Soal Nomor 3

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('produk', function () {
+    $kodeproduk = ['BRG001', 'BRG002'];
+    $namaproduk = ['Buku', 'Pena'];
+    $jenisproduk = ['Alat Tulis', 'Alat Tulis'];
+    $harga = [20000, 15000];
+    $jumlah = count($kodeproduk);
+    return view('produk', compact('kodeproduk', 'namaproduk', 'jenisproduk', 'harga', 'jumlah'));
+});
